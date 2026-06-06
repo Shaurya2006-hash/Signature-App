dfc37fe48c00cccdd5cc5cffbafd2f6325bb6480
Shaurya2006-hash <Shauryaaggnoida1@gmail.com>
2026-06-06 17:29:25 +0530

    Day 2 Task

---
+++ Day-2/server1/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
