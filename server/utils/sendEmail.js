d50f9e9a3b3e6f552653f1243ffafd39f89cc947
Shaurya2006-hash <Shauryaaggnoida1@gmail.com>
2026-06-18 18:06:45 +0530

    Day-13 Task Frontend and Backend Deployment

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,33 +1,26 @@
-const nodemailer =
-  require("nodemailer");
-
-const sendEmail = async (
-  to,
-  subject,
-  text
-) => {
-  const transporter =
-    nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user:
-          process.env.EMAIL_USER,
-        pass:
-          process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-
+const nodemailer = require("nodemailer");
+ 
+const sendEmail = async (to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+ 
   await transporter.sendMail({
-    from:
-      process.env.EMAIL_USER,
+    from: process.env.EMAIL_USER,
     to,
     subject,
     text,
   });
 };
-
-module.exports =
-  sendEmail;
+ 
+module.exports = sendEmail;
+ 
